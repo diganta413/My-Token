a5f6f40afd718882db0163a1544682c7e7312dc8
diganta413 <ghoshdiganta226@gmail.com>
2021-10-23 21:38:02 +0530

    Balances added

--- contracts/Token.sol
+++ contracts/Token.sol
@@ -1,10 +1,16 @@
 pragma solidity >=0.4.22 <0.9.0;
 
 contract Token{
-  uint256 public totalSupply;
+  uint256 public _totalSupply;
+  mapping(address => uint256) public _balances;
+  string public _name;
+  string public _symbol;
 
-  constructor() public{
-	  totalSupply = 10000;
+  constructor(uint256 initalSupply) public{
+	  _name = "Digu's Token";
+	  _symbol = "Digu";
+	  _totalSupply = initalSupply;
+	  _balances[msg.sender] = initalSupply;
   }  
 
 }
--- test/Token.js
+++ test/Token.js
@@ -3,8 +3,16 @@ const Token = artifacts.require("../contracts/Token.sol");
 contract("Token", function(accounts){
 	it("sets the total amount on deployment",async  function() {
 		const instance = await Token.deployed();
-		const totalSupply = await instance.totalSupply();
-		assert.equal(totalSupply,10000,"10000 tokens is not stored");
+		const totalSupply = await instance._totalSupply();
+		assert.equal(totalSupply,100000,"100000 tokens is not stored");
+	})
+
+	it("name and symbol set correctly",async function() {
+		const instance = await Token.deployed();
+		const name = await instance._name();
+		const symbol = await instance._symbol();
+		assert.equal(name,"Digu's Token","Name is not set correctly");
+		assert.equal(symbol,"Digu",symbol);
 	})
 }
 )
